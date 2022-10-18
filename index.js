5f102315bcef052ac4d24ae3d75b12cfc841c640
reGrach <regrachdev@yandex.ru>
2022-10-18 18:40:55 +0300

    Init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect('/index.html')
+})
+
+app.post('/login', (req, res) => {
+    res.send('Вход выполнен!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
